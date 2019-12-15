7edc5a4169ecfc434aab2674446468a462a903d9
Louis-crypto <kinglouis745@gmail.com>
2019-12-15 19:38:33 +0300

    add a delivery function

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,20 @@ function totalPrice (){
   event.preventDefault();
 };
 
-// $("#prodIcon").on("click", function () {
-//   $("#product").show();
-//   $("#prodIcon").hide();
-// });
+function totalPrice1 (){
+  var price = 0;
+  var selectedSize = document.getElementById("sizes")
+  var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
+  var selectedCrust = document.getElementById("crusts")
+  var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
+  var selectedTopping = document.getElementById("toppings")
+  var CostOfToppings = selectedTopping.options[selectedCrust.selectedIndex].value;
+  var QuantityOfPizza = document.getElementById("numberOfPizzas").value;
+  price = parseInt(CostOfSize) + parseInt(CostOfCrust) + parseInt(CostOfToppings)
+  totalPrice = price * parseInt(QuantityOfPizza);
+  document.getElementById("total").value=totalPrice;
+  event.preventDefault();
+};
 
 $(document).ready (function(){
 
@@ -31,6 +41,10 @@ $("#makeOrder").on("click", function(){
 
 $("#deliver").on("click", function(){
   $("#menu1").fadeIn(1000);
+})
+
+$("#order").on("click", function(){
+  $("#menu1").hide();
 })
 
 });
