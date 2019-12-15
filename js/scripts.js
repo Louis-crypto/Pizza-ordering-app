afb58037c213f403ba97cd8a41c3ebce822b5ef1
Louis-crypto <kinglouis745@gmail.com>
2019-12-15 12:48:58 +0300

    add code to get price of pizza by size

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// var size = function run() {
+//   document.getElementById("total").value = document.getElementById("sizes").value;
+// };
+
+var sizePrice = document.getElementById("sizes").value;
+var crustPrice = document.getElementById("crusts").value;
