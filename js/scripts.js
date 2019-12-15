8dda9aef3e0dcd14e08d6c86bed2e50a74b14d4a
Louis-crypto <kinglouis745@gmail.com>
2019-12-15 15:38:40 +0300

    add image to the title

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-
 function totalPrice (){
   var price = 0;
   var selectedSize = document.getElementById("sizes")
   var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
   var selectedCrust = document.getElementById("crusts")
   var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
-  var selectedToppings = document.getElementById("toppings")
+  var selectedTopping = document.getElementById("toppings")
+  var CostOfToppings = selectedTopping.options[selectedCrust.selectedIndex].value;
   var QuantityOfPizza = document.getElementById("numberOfPizzas").value;
-  price = parseInt(CostOfSize) + parseInt(CostOfCrust) * parseInt(QuantityOfPizza);
-  // return (isNaN(price)) ? 0 : price;
-  alert(price);
+  price = parseInt(CostOfSize) + parseInt(CostOfCrust) + parseInt(CostOfToppings)
+  totalPrice = price * parseInt(QuantityOfPizza);
+  document.getElementById("total").value=totalPrice;
+  event.preventDefault();
 };
