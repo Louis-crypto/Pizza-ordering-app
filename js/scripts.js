aba7131423ec9767ef601975edf7d18d86b362a0
Louis-crypto <kinglouis745@gmail.com>
2019-12-15 15:01:42 +0300

    add code to calculate cost of each pizza size plus toppins and crust

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 
 function totalPrice (){
   var price = 0;
-  var selectedSize = document.getElementById('sizes')
-  var AmountOfSize = selectedSize.options[selectedSize.selectedIndex].value;
-  var QuantityOfPizza = document.getElementById('numberOfPizzas').value;
-  price = parseInt(QuantityOfPizza) * parseInt(AmountOfSize);
+  var selectedSize = document.getElementById("sizes")
+  var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
+  var selectedCrust = document.getElementById("crusts")
+  var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
+  var selectedToppings = document.getElementById("toppings")
+  var QuantityOfPizza = document.getElementById("numberOfPizzas").value;
+  price = parseInt(CostOfSize) + parseInt(CostOfCrust) * parseInt(QuantityOfPizza);
   // return (isNaN(price)) ? 0 : price;
   alert(price);
 };
