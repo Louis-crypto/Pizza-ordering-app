45576571b61acb6c3161ef92b03dcd561cadf187
Louis-crypto <kinglouis745@gmail.com>
2019-12-16 08:24:51 +0300

    change color and font-size of about section

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,13 @@ function totalPrice (){
 
 function totalPrice1 (){
   var price = 0;
-  var selectedSize = document.getElementById("sizes")
+  var selectedSize = document.getElementById("sizes1")
   var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
-  var selectedCrust = document.getElementById("crusts")
+  var selectedCrust = document.getElementById("crusts1")
   var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
-  var selectedTopping = document.getElementById("toppings")
+  var selectedTopping = document.getElementById("toppings1")
   var CostOfToppings = selectedTopping.options[selectedCrust.selectedIndex].value;
-  var QuantityOfPizza = document.getElementById("numberOfPizzas").value;
+  var QuantityOfPizza = document.getElementById("numberOfPizzas1").value;
   price = parseInt(CostOfSize) + parseInt(CostOfCrust) + parseInt(CostOfToppings)
   totalPrice = price * parseInt(QuantityOfPizza);
   document.getElementById("totalDelivery").value = totalPrice;
